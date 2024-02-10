833b051a0a557bc01e0f9a2dcadf082fd647983a
Alfredo Lorentiar S <alorentiar03@gmail.com>
2024-02-10 11:07:40 +0700

    add shell nix

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let 
+    nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+    pkgs = import nixpkgs {config = {} ; overlays = []; };
+in
+
+pkgs.mkShell {
+    packages = with pkgs; [
+        python3
+        lolcat
+    ];
+
+    shellHook = ''
+    python hello.py | lolcat
+    '';
+}
